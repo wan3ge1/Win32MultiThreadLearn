3d195cf4812a2e38efa24fb6ca24a68442926541
wan3ge1 <182803721@qq.com>
2021-05-14 16:16:14 +0800

    20210514study

--- Charpter2/Charpter2/demo.cpp
+++ Charpter2/Charpter2/demo.cpp
@@ -1,9 +1,33 @@
-#include <iostream>
+#define WIN32_LEAN_AND_MEAN 
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
 
-using namespace std;
+DWORD WINAPI ThreadFunc(LPVOID);
 
 int main()
 {
-	cout << "hello world." << endl;
+	HANDLE hThrd;
+	DWORD threadId;
+
+	for (int i = 0; i < 5; i++)
+	{
+		hThrd =  CreateThread(NULL, 0, ThreadFunc, (LPVOID)i, 0, &threadId);
+		if (hThrd)
+		{
+			printf("Thread lauched %d\n", i);
+			CloseHandle(hThrd);
+		}
+	}
+	Sleep(2000);
+
+
+	return 0;
+}
+
+DWORD WINAPI ThreadFunc(LPVOID n)
+{
+	for (int i = 0; i < 10; i++)
+		printf("%d%d%d%d%d%d%d%d%d%d\n", n, n, n, n, n, n, n, n, n, n);
 	return 0;
 }
